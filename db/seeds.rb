0dd2078793a363ec39a6f384b5bbc81270be6c72
mkarkkainen <mikko.karkkainen@gmail.com>
2016-06-04 17:12:48 +0300

    Reseeded database

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,30 +6,11 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-User.create!(first_name: "Example",
-             last_name:  "User",
+User.create!(first_name: "Mikko",
+             last_name:  "Kärkkäinen",
              email: "asdf@railstutorial.org",
              password:              "foobars",
              password_confirmation: "foobars",
              admin:     true,
              activated: true,
              activated_at: Time.zone.now)
-
-99.times do |n|
-  first_name = last_name  = Faker::Name.name
-  email = "example-#{n}@railstutorial.org"
-  password = "password"
-  User.create!(first_name:  first_name,
-              last_name: last_name,
-              email: email,
-              password:              password,
-              password_confirmation: password,
-              activated: true,
-              activated_at: Time.zone.now)
-end
-
-users = User.order(:created_at).take(6)
-50.times do
-  content = Faker::Lorem.sentence(5)
-  users.each { |user| user.topics.create!(content: content) }
-end
