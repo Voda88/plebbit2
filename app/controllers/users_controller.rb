4825ca9d1eb2f79aa39a6dc8cd3f44f2799d2a83
mkarkkainen <mikko.karkkainen@gmail.com>
2016-06-07 08:19:24 +0300

    Fixed user controller strong params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -51,6 +51,10 @@ class UsersController < ApplicationController
     redirect_to users_url
   end
 
-  
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name,:last_name,:email,:password,:password_confirmation)
+  end
 
 end
