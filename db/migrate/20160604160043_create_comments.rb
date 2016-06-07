59ec3acffd31f7509adee450fb300d234c6447a9
mkarkkainen <mikko.karkkainen@gmail.com>
2016-06-07 06:38:02 +0300

    fixed database migrations

--- db/migrate/20160604160043_create_comments.rb
+++ db/migrate/20160604160043_create_comments.rb
@@ -1,9 +1,12 @@
 class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
-      t.text :content
-      t.string :title
-      t.timestamps null: false
+		t.text :content
+		t.string :title
+		t.timestamps null: false
+		t.integer :commentable_id
+		t.integer  :user_id
+		t.integer  :topic_id
     end
     add_index :comments, :commentable_id
   end
