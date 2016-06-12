74de6e5bca6c46e01cec8f66a574e52b393a7846
mkarkkainen <mikko.karkkainen@gmail.com>
2016-06-12 05:27:59 +0300

    Prevented users from voting on their own posts

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -5,13 +5,18 @@ class VotesController < ApplicationController
 		@user=current_user
 		find_voteable
 		@vote=@voteable.votes.build(vote_params)
-		if @vote.save
-			flash[:success] = "succesful vote"
-			redirect_to :back
+		unless @voteable.user_id == @user.id
+			if @vote.save
+				flash[:success] = "succesful vote"
+				redirect_to :back
+			else
+				flash[:danger] = "You have already voted"
+				redirect_to :back
+			end	
 		else
-			flash[:danger] = "You have already voted"
+			flash[:danger] = "You cannot vote on your own posts"
 			redirect_to :back
-		end	
+		end
 	end
 
 	def update
